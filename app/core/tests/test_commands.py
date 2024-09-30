e0d360e3bb99394278812fa3876a414741348149
aswin <aswin.k@webandcrafts.com>
2024-09-30 16:05:05 +0530

    github actions

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,31 +1,34 @@
-from unittest.mock import patch  #mocking the db behaviour
-
-from psycopg2 import OperationalError as Psycopg2Error #it is the error getting before connecting the db
-
-from django.core.management import call_command #call the command by name that is testing 
+from unittest.mock import patch  # mocking the db behavior
+from psycopg2 import OperationalError as Psycopg2Error  # error raised before connecting to the DB
+from django.core.management import call_command  # to call the management command
 from django.db.utils import OperationalError
-from django.test import SimpleTestCase #base test class for testing
+from django.test import SimpleTestCase  # base test class
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    
+
     def test_wait_for_db_ready(self, patched_check):
-        patched_check.return_value = True  # Mock the check function to return True
-        
+        """Test waiting for db when db is available."""
+        patched_check.return_value = True  # mock the check function to return True
+
         # Call the command
         call_command('wait_for_db')
-        
-        # Check if the mock function was called once
+
+        # Check if the check function was called once
         patched_check.assert_called_once_with(databases=['default'])
 
-    @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_sleep,patched_check):
+    @patch('time.sleep')  # Mock the time.sleep to avoid delay
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for db with delay and retries."""
+        # Simulate errors in the first 5 calls, then success on the 6th call
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 + [True]
-        
-        call_command('wait_for_db')
-        
-        self.assertEqual(patched_check.call_count,6)
-        patched_check.assert_called_once_with(databases=['default'])
+                                    [OperationalError] * 3 + [True]
 
-        
+        # Call the command
+        call_command('wait_for_db')
+
+        # Check that the check function was called 6 times
+        self.assertEqual(patched_check.call_count, 6)
+
+        # Check if the final call was made with the correct argument
+        patched_check.assert_called_with(databases=['default'])
