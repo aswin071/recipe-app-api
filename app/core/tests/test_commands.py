f4b2df7a88d74231f32b943093aa55ce31f1e66e
aswin <aswin.k@webandcrafts.com>
2024-09-30 16:02:16 +0530

    github actions

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -16,7 +16,8 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
         
         # Check if the mock function was called once
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep,patched_check):
         patched_check.side_effect = [Psycopg2Error] * 2 + \
@@ -25,5 +26,6 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
         
         self.assertEqual(patched_check.call_count,6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
+
         
